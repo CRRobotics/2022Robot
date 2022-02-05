08303844292e808fe33af63319c9747bb53d16ab
ColbyEB <colby.bittner@icsd.k12.ny.us>
2022-02-05 13:43:51 -0500

    Update Climb.java

--- src/main/java/org/team639/subsystems/Climb.java
+++ src/main/java/org/team639/subsystems/Climb.java
@@ -1,10 +1,24 @@
 package org.team639.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Climb extends SubsystemBase {
-    public Climb() {}
+public class Climb extends SubsystemBase
+{
+    private VictorSPX winch1;
+    private VictorSPX winch2;
+    private  linearLeft;
+    private  linearRight;
+
+    public Climb()
+    {
+        winch1 = new VictorSPX();
+        winch2 = new VictorSPX();
+        linearLeft = new VictorSPX();
+        linearLeft = new VictorSPX();
+    }
     @Override
     public void periodic() {}
 }
