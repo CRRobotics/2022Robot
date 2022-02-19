80fd6c9e39b250d60d16e4c0750d7e652568ef1c
asun121 <albert.yang.sun@gmail.com>
2022-02-19 13:50:56 -0500

    Added toggle gears command

---
+++ src/main/java/org/team639/commands/Drive/ToggleGears.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.team639.commands.Drive;
+
+import org.team639.lib.states.GearMode;
+import org.team639.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ToggleGears extends CommandBase {
+  private DriveTrain driveTrain;
+  /** Creates a new ToggleGears. */
+  public ToggleGears(DriveTrain driveTrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.driveTrain = driveTrain;
+    addRequirements(driveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    System.out.println("ToggleGears initialized");
+    if(driveTrain.getGearMode() == GearMode.low)
+      driveTrain.toggleGearHigh();
+    else
+      driveTrain.toggleGearLow();
+
+  }
+
+  public void end(boolean interrupted)
+  {
+    System.out.println("Gear shift, shifting outta here");
+  }
+
+}
