f454243ca126b2686ab5ace522ca4dfa92355b7a
ColbyEB <colby.bittner@icsd.k12.ny.us>
2022-02-21 20:54:25 -0500

    Update ValueFromDistance.java

--- src/main/java/org/team639/lib/math/ValueFromDistance.java
+++ src/main/java/org/team639/lib/math/ValueFromDistance.java
@@ -13,9 +13,8 @@ public class ValueFromDistance
     {
         double linearInterpolation = distance % 1.0;
         return new AngleSpeed(
-                shootMap.get((int)distance).getAngle() + linearInterpolation * ((shootMap.get((int)distance + 1).getAngle() - shootMap.get((int)distance).getAngle()) / 2),
-                shootMap.get((int)distance).getSpeed() + linearInterpolation * ((shootMap.get((int)distance + 1).getSpeed() - shootMap.get((int)distance).getSpeed()) / 2)
-        );
+                shootMap.get((int)distance).getAngle() + linearInterpolation * (shootMap.get((int)distance + 1).getAngle() - shootMap.get((int)distance).getAngle()),
+                shootMap.get((int)distance).getSpeed() + linearInterpolation * (shootMap.get((int)distance + 1).getSpeed() - shootMap.get((int)distance).getSpeed()));
 
 
 
