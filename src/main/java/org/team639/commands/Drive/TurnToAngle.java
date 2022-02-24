0e0a21bec58f8a28287874920c038e48dd802ec1
asun121 <albert.yang.sun@gmail.com>
2022-02-24 14:51:35 -0500

    Added TurnToAngle. May be better than autorotation

---
+++ src/main/java/org/team639/commands/Drive/TurnToAngle.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.team639.commands.Drive;
+
+import org.team639.subsystems.DriveTrain;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class TurnToAngle extends CommandBase {
+  private DriveTrain driveTrain;
+  private PIDController turnController;
+  private double setpoint;
+  private boolean clockwise;
+
+  /** Creates a new TurnToAngle. */
+  public TurnToAngle(DriveTrain driveTrain, double angle) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.driveTrain = driveTrain;
+    this.turnController = driveTrain.getTurnController();
+    addRequirements(driveTrain);
+    angle %= 360;
+
+    this.clockwise = Math.signum(angle) > 0 ? true : false;
+    setpoint = Math.abs(angle) + driveTrain.getHeading();
+    driveTrain.getTurnController().setSetpoint(setpoint);
+
+    turnController.setTolerance(0.05);
+    turnController.setSetpoint(setpoint);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    driveTrain.setSpeedsPercent(0, 0);
+    System.out.println("Auto rotate initializing");
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double currMultiplier = turnController.calculate(driveTrain.getHeading(), setpoint);
+    if(clockwise)
+    {
+      driveTrain.setSpeedsPercent(-1 * currMultiplier, 1 * currMultiplier);
+    }
+    else
+    {
+      driveTrain.setSpeedsPercent(currMultiplier, -1 * currMultiplier);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveTrain.setSpeedsPercent(0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if(turnController.atSetpoint())
+      return true;
+    return false;
+  }
+}
